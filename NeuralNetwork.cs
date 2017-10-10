4ff401b728d78a2563115044c13ee3f0ff0dc620
RudyMeijer <RudyMeijer@gmail.com>
2017-10-10 22:32:41 +0200

    Intermediate Back Propagation

--- NeuralNetwork.cs
+++ NeuralNetwork.cs
@@ -35,7 +35,7 @@ namespace NeuralNetwork
 		/// <param name="maxEpochs"></param>
 		/// <param name="learnRate"></param>
 		/// <returns></returns>
-		internal double[] Train(double[] trainData, int maxEpochs, double learnRate)
+		internal double[,] Train(double[] trainData, int maxEpochs, double learnRate)
 		{
 			var mse = 1.0;
 			int epoch = 0;
@@ -45,12 +45,90 @@ namespace NeuralNetwork
 				this.Inputs = SetInputs(trainData, idx);
 				this.ExpectedOutput = trainData[Inputs.Length + idx];
 				ComputeOutputs();
-				//UpdateWeights();
-				//mse = //GetMeansSquareError();
-				epoch++; idx++;
+				BackPropagation(this.ExpectedOutput, learnRate);//UpdateWeights();
+																//mse = //GetMeansSquareError();
+				++epoch; ++idx;
 			}
 
-			return null;//GetWeigths();
+			return GetWeigths();
+		}
+
+		/// <summary>
+		/// Update the weights and biases using back-propagation.
+		/// </summary>
+		private void BackPropagation(double expectedOutput, double learnRate)
+		{
+			//
+			// 1. compute output gradients
+			//
+			var oGrads = new Double[Output.Length];
+			var hGrads = new Double[Hidden.Length];
+			for (int i = 0; i < oGrads.Length; ++i)
+			{
+				//For sigmoid activation, the derivative of y = log-sigmoid(x) is y * (1 - y)
+				double derivative = Output[i] * (1 - Output[i]);
+				// 'mean squared error version' includes (1-y)(y) derivative
+				oGrads[i] = derivative * (ExpectedOutput - Output[i]);
+			}
+
+			// 2. compute hidden gradients
+			for (int i = 0; i < hGrads.Length; ++i)
+			{
+				// derivative of tanh = (1 - y) * (1 + y)
+				double derivative = Output[i] * (1 - Output[i]);
+				double sum = 0.0;
+				for (int j = 0; j < Output.Length; ++j) // each hidden delta is the sum of numOutput terms
+				{
+					double x = oGrads[j] * hiddenNeurons[i].Weights[j];// hoWeights[i][j];
+					sum += x;
+				}
+				hGrads[i] = derivative * sum;
+			}
+			// 3a. update hidden weights (gradients must be computed right-to-left but weights
+			// can be updated in any order)
+			//for (int i = 0; i < ihWeights.Length; ++i) // 0..2 (3)
+			//{
+			//	for (int j = 0; j < ihWeights[0].Length; ++j) // 0..3 (4)
+			//	{
+			//		double delta = learnRate * hGrads[j] * inputs[i]; // compute the new delta
+			//		ihWeights[i][j] += delta; // update. note we use '+' instead of '-'. this can be very tricky.
+			//								  // now add momentum using previous delta. on first pass old value will be 0.0 but that's OK.
+			//		ihWeights[i][j] += momentum * ihPrevWeightsDelta[i][j];
+			//		ihWeights[i][j] -= (weightDecay * ihWeights[i][j]); // weight decay
+			//		ihPrevWeightsDelta[i][j] = delta; // don't forget to save the delta for momentum 
+			//	}
+			//}
+
+			//// 3b. update hidden biases
+			//for (int i = 0; i < hBiases.Length; ++i)
+			//{
+			//	double delta = learnRate * hGrads[i] * 1.0; // t1.0 is constant input for bias; could leave out
+			//	hBiases[i] += delta;
+			//	hBiases[i] += momentum * hPrevBiasesDelta[i]; // momentum
+			//	hBiases[i] -= (weightDecay * hBiases[i]); // weight decay
+			//	hPrevBiasesDelta[i] = delta; // don't forget to save the delta
+			//}
+
+		}
+
+		private double[,] GetWeigths()
+		{
+			double[,] weights = new Double[hiddenNeurons.Count, Inputs.Length];
+			for (int i = 0; i < hiddenNeurons.Count; i++)
+			{
+				for (int j = 0; j < Inputs.Length; j++)
+				{
+					weights[i, j] = hiddenNeurons[i].Weights[j];
+				}
+			}
+			for (int i = 0; i < outputNeurons.Count; i++)
+			{
+				for (int j = 0; j < Hidden.Length; j++)
+				{
+					weights[i, j] = outputNeurons[i].Weights[j];
+				}
+			}
+			return weights;
 		}
 
 		private void ComputeOutputs()
